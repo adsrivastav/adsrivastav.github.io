c40ca3c2675d8b625e0c5798511dc493ff301f76
Adarsh Srivastav <52306956+adsrivastav@users.noreply.github.com>
2019-06-29 11:46:01 +0530

    intial attempt

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+        $('#slides').superslides({
+         animation: 'fade',
+         play: 3000,
+         pagination: false
+
+        });
+var typed  = new Typed(".typed", {
+	strings: ["Software Engineer", "Web Developer", "Coder"],
+	typeSpeed: 70,
+	loop: true,
+	startDelay: 1000,
+	showCursor: false
+
+
+
+
+});
+
+		
+	  
+});
