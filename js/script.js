37bec5efec136d3aecc3c3677efc2499ff44e681
Adarsh Srivastav <52306956+adsrivastav@users.noreply.github.com>
2019-07-01 13:40:06 +0530

    carosal prob solve and stat modified

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-$(document).ready(function(){
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+})
+
+
+  $(document).ready(function(){
 
         $('#slides').superslides({
-         animation: 'fade',
-         play: 3000,
-         pagination: false
+            animation: 'fade',
+            play: 3000,
+            pagination: false
 
+             });
+		var typed  = new Typed(".typed", {
+			strings: ["Software Engineer", "Web Developer", "Coder"],
+			typeSpeed: 70,
+			loop: true,
+			startDelay: 1000,
+			showCursor: false
+
+		     });
+        $('.owl-carousel').owlCarousel({
+        	loop: true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+     	});
+
+       $('.chart').easyPieChart({
+            easing: 'easeinout',
+            barColor: '#fff',
+            trackColor: false,
+            scaleColor: false,
+            lineWidth: 4,
+            size: 152,
+            onStep: function(from, to, percent){
+              $(this.el).find('.percent').text(Math.round(percent))
+
+            }
         });
-var typed  = new Typed(".typed", {
-	strings: ["Software Engineer", "Web Developer", "Coder"],
-	typeSpeed: 70,
-	loop: true,
-	startDelay: 1000,
-	showCursor: false
-
-
-
-
-});
 
 		
 	  
